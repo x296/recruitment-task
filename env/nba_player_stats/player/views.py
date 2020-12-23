b8a26f14b93223b469e2bde9be0491774f14db7a
Piotr Żyziński <33286821+x296@users.noreply.github.com>
2020-12-23 03:15:33 +0100

    beggining of simple styling

--- env/nba_player_stats/player/views.py
+++ env/nba_player_stats/player/views.py
@@ -1,3 +1,26 @@
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+
+	url = 'https://www.balldontlie.io/api/v1/players/{}'
+	player_id = 237
+
+	r = requests.get(url.format(player_id)).json()
+
+	player_stats = {
+		"id": r['id'],
+		"first_name": r['first_name'],
+		"last_name": r['last_name'],
+		"position": r['position'],
+		"height_feet": r['height_feet'],
+		"height_inches": r['height_inches'],
+		"weight_pounds": r['weight_pounds'],
+		"team_name": r['team']['full_name'],
+		"conference": r['team']['conference'],
+		"division": r['team']['division'],
+	}
+
+	context = {'player_stats': player_stats}
+
+	return render(request, 'player/player.html', context)
