eab887d0ae4bb843cc33ae99cd6c9adf133a59ce
Piotr Żyziński <33286821+x296@users.noreply.github.com>
2020-12-23 23:42:39 +0100

    created model for player and display all cards

--- env/nba_player_stats/player/views.py
+++ env/nba_player_stats/player/views.py
@@ -1,26 +1,37 @@
+# from random import randint
 import requests
 from django.shortcuts import render
+from .models import Stats
 
 def index(request):
 
-	url = 'https://www.balldontlie.io/api/v1/players/{}'
-	player_id = 237
+	players_list = Stats.objects.all()
 
-	r = requests.get(url.format(player_id)).json()
+	# default_ids = [15, 274, 237, 192, 246, 145, 132, 278, 79]
 
-	player_stats = {
-		"id": r['id'],
-		"first_name": r['first_name'],
-		"last_name": r['last_name'],
-		"position": r['position'],
-		"height_feet": r['height_feet'],
-		"height_inches": r['height_inches'],
-		"weight_pounds": r['weight_pounds'],
-		"team_name": r['team']['full_name'],
-		"conference": r['team']['conference'],
-		"division": r['team']['division'],
-	}
+	# players_list = []
 
-	context = {'player_stats': player_stats}
+	# url = 'https://www.balldontlie.io/api/v1/players/{}'
+
+	# for player_id in default_ids:
+	# 	r = requests.get(url.format(player_id)).json()
+
+	# 	player_stats = {
+	# 		"id": r['id'],
+	# 		"first_name": r['first_name'],
+	# 		"last_name": r['last_name'],
+	# 		"position": r['position'],
+	# 		"height_feet": r['height_feet'],
+	# 		"height_inches": r['height_inches'],
+	# 		"weight_pounds": r['weight_pounds'],
+	# 		"team_name": r['team']['full_name'],
+	# 		"conference": r['team']['conference'],
+	# 		"division": r['team']['division'],
+	# 	}
+
+	# 	players_list.append(player_stats)
+
+	# context = {'player_stats': player_stats}
+	context = {'players_list': players_list}
 
 	return render(request, 'player/player.html', context)
