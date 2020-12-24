20232b5af26fce3e8257d22bab7c2f82fa428433
Piotr Żyziński <33286821+x296@users.noreply.github.com>
2020-12-24 02:00:15 +0100

    end of time, all other ideas will be in readme file

--- env/nba_player_stats/player/views.py
+++ env/nba_player_stats/player/views.py
@@ -1,37 +1,43 @@
-# from random import randint
+from random import randint
 import requests
 from django.shortcuts import render
 from .models import Stats
 
 def index(request):
 
-	players_list = Stats.objects.all()
+	# players_list = Stats.objects.all()
 
 	# default_ids = [15, 274, 237, 192, 246, 145, 132, 278, 79]
 
-	# players_list = []
+	players_list = []
 
-	# url = 'https://www.balldontlie.io/api/v1/players/{}'
+	url = 'https://www.balldontlie.io/api/v1/players/{}'
 
-	# for player_id in default_ids:
-	# 	r = requests.get(url.format(player_id)).json()
+	for _ in range(9):
+		r = None
+		while r is None:
+			try:
+				r = requests.get(url.format(randint(1, 300))).json()
+			except:
+				pass
 
-	# 	player_stats = {
-	# 		"id": r['id'],
-	# 		"first_name": r['first_name'],
-	# 		"last_name": r['last_name'],
-	# 		"position": r['position'],
-	# 		"height_feet": r['height_feet'],
-	# 		"height_inches": r['height_inches'],
-	# 		"weight_pounds": r['weight_pounds'],
-	# 		"team_name": r['team']['full_name'],
-	# 		"conference": r['team']['conference'],
-	# 		"division": r['team']['division'],
-	# 	}
 
-	# 	players_list.append(player_stats)
+		player_stats = {
+			"id": r['id'],
+			"first_name": r['first_name'],
+			"last_name": r['last_name'],
+			"position": r['position'][0],
+			"height_feet": r['height_feet'],
+			"height_inches": r['height_inches'],
+			"weight_pounds": r['weight_pounds'],
+			"team_name": r['team']['full_name'],
+			"conference": r['team']['conference'],
+			"division": r['team']['division'],
+		}
 
-	# context = {'player_stats': player_stats}
+		players_list.append(player_stats)
+		r = None
+
 	context = {'players_list': players_list}
 
 	return render(request, 'player/player.html', context)
